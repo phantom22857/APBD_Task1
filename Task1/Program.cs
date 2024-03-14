9c5898e5d0db197aed647c2c107d1ea6e41b387a
s27180 <s27180@pjwstk.edu.pl>
2024-03-14 21:10:31 +0100

    Modification1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static double GetAverage(int[] numbers)
+{
+    double sum = 0.0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return sum / numbers.Length;
+}
+
+int[] numbers = { 1, 2, 3, 4 };
+double average = GetAverage(numbers);
+Console.WriteLine(average);
